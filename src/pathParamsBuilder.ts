e77f047f719bbdd9b809e89bbda05dfca4dd0778
Jeffrey van Hoven <contact@jvhoven.nl>
2019-03-22 09:45:15 +0100

    Fix pathParamsBuilder JSDoc typo

--- src/pathParamsBuilder.ts
+++ src/pathParamsBuilder.ts
@@ -21,7 +21,7 @@ function replace(key: string, value: string, url: string): string {
  * Replaces all the matching keys in the path params in the url with the value.
  *
  * @example
- * pathParamsBuilder({url: '/users/:id', pathParams: {idd: 1}}) === '/users/1
+ * pathParamsBuilder({url: '/users/:id', pathParams: {id: 1}}) === '/users/1
  *
  * @param {String} options.url The url to be parsed
  * @param {Object} options.pathParams Consists of the matching key in the url to be replaced by the value.
