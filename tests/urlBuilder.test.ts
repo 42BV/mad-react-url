12a0d352e91dc405e38343603a9aac60a411df87
Maarten Hus <maarten.hus@42.nl>
2019-08-07 16:15:59 +0200

    Bugfix: the type of the pathParams was incorrectly set to QueryParam.
    Before the type of the urlBuilder was:
    
    ```ts
    export interface UrlBuilderOptions<QueryParams> {
      url: Url;
      pathParams?: QueryParams;
      queryParams?: QueryParams;
      defaultQueryParams?: QueryParams;
    }
    ```
    
    This is incorrect because `pathParams` is never the same as `queryParams`.
    
    The correct type is:
    
    ```ts
    export interface UrlBuilderOptions<PathParams, QueryParams> {
      url: Url;
      pathParams?: PathParams;
      queryParams?: QueryParams;
      defaultQueryParams?: QueryParams;
    }
    ```
    
    The PathParams should have its own generic to make this correct.
    
    Appearently this is something that TypeScript `3.5.3` can pick up,
    so bumpbed the version to it.
    
    Also wrote a scenario test which makes sure that the TypeScript will
    fail in the compile if this bug ever comes back.

--- tests/urlBuilder.test.ts
+++ tests/urlBuilder.test.ts
@@ -1,4 +1,5 @@
 import { urlBuilder } from '../src/urlBuilder';
+import { Url } from '../src/models';
 
 describe('urlBuilder', () => {
   it('should know how to return abstract urls', () => {
@@ -36,4 +37,41 @@ describe('urlBuilder', () => {
 
     expect(urlBuilder({ url, pathParams, queryParams, defaultQueryParams })).toBe(expected);
   });
+
+  test('scenario which testes the typescript types', () => {
+    // We are testing the typescript types here so a failure means
+    // typscript will complain about the following code.
+
+    interface DashboardPathParams {
+      id: number;
+    }
+    
+    interface DashboardQueryParams {
+      page: number;
+      query: string;
+    }
+
+    function defaultDashboardQueryParams(): DashboardQueryParams {
+      return {
+        page: 1,
+        query: ''
+      };
+    }
+
+    function toDashboard(
+      pathParams?: DashboardPathParams,
+      queryParams?: Partial<DashboardQueryParams>
+    ): Url {
+      return urlBuilder({
+        url: '/dashboard/:id',
+        pathParams,
+        queryParams,
+        defaultQueryParams: defaultDashboardQueryParams()
+      });
+    }
+
+    expect(toDashboard()).toBe('/dashboard/:id');
+    expect(toDashboard({ id: 42 })).toBe('/dashboard/42');
+    expect(toDashboard({ id: 42 }, { page: 1337})).toBe('/dashboard/42?page=1337');
+  })
 });
