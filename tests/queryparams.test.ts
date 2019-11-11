23566c7a6a391f69ce4bedbfc9489ec90de7f892
Maarten Hus <maarten.hus@42.nl>
2019-11-11 13:54:20 +0100

    fix: booleans from default query params were inverted.
    When parsing empty query params with the following defaults:
    
    ```ts
    function defaultQueryParams() {
      return { visible: true },
    };
    ```
    
    Visible would be `false` and not the expected `true`.
    
    The reason for this is because when the default value was used due to
    a missing actual query parameters value, it also got parsed as if it
    was a string.
    
    Now whenever we detect that the value is a default value we forgo any
    parsing / conversion to an actual value, since that is not needed for
    a default value.
    
    In the tests we now test the conversion with an integration test. This
    way we test from userspace -> query-params -> mad-react-url. This way
    we can catch nasty integration bugs quicker.
    
    Furthermore the wrong `query params` library was used. Instead of
    `query-params` the Node.js standard library module `queryparams` was
    used.
    
    Also upgraded the queryparams library to the latest version.
    
    Closes #19

--- tests/queryparams.test.ts
+++ tests/queryparams.test.ts
@@ -1,4 +1,4 @@
-import { queryParamsFromLocation, convertQueryParamsToConcreteType } from '../src/queryparams';
+import { queryParamsFromLocation } from '../src/queryparams';
 
 describe('queryParamsFromLocation', () => {
   it('should know how to convert strings that start with a question mark', () => {
@@ -12,106 +12,167 @@ describe('queryParamsFromLocation', () => {
 
     expect(queryParamsFromLocation(location, { query: 'default' }, 'debug')).toEqual({ query: 'hallo' });
   });
-});
 
-describe('convertQueryParamsToConcreteType', () => {
-  it('should warn when default query parameters does not contain a query param', () => {
-    jest.spyOn(console, 'warn').mockImplementation(() => undefined);
+  it('should fallback to default query parameters when params are misssing', () => {
+    const location = { search: '' };
+    const defaultQueryParams = {
+      number: 42,
+      numbers: [1, 2, 3],
+      float: 1.42,
+      floats: [1, 2, 3],
+      string: 'small',
+      strings: ['small', 'large'],
+      falseBoolean: false,
+      trueBoolean: true,
+      booleans: [false, true, false],
+    };
 
-    const queryParams = convertQueryParamsToConcreteType({ color: 'red' }, {}, 'AppComponent');
-    expect(queryParams).toEqual({ color: 'red' });
+    const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+    expect(queryParams).toEqual({
+      number: 42,
+      numbers: [1, 2, 3],
+      float: 1.42,
+      floats: [1, 2, 3],
+      string: 'small',
+      strings: ['small', 'large'],
+      falseBoolean: false,
+      trueBoolean: true,
+      booleans: [false, true, false],
+    });
 
-    expect(console.warn).toHaveBeenCalledTimes(1);
-    expect(console.warn).toHaveBeenCalledWith(
-      `@42.nl/react-url: no default query param defined for "color" for: "AppComponent".`,
-    );
+    // Check reference equality of arrays, that they are actually the
+    // exact same value in memory.
+    expect(queryParams.numbers).toBe(defaultQueryParams.numbers);
+    expect(queryParams.floats).toBe(defaultQueryParams.floats);
+    expect(queryParams.strings).toBe(defaultQueryParams.strings);
+    expect(queryParams.booleans).toBe(defaultQueryParams.booleans);
   });
 
-  it('should leave strings and unhandled types alone', () => {
-    const queryParams = convertQueryParamsToConcreteType({ color: 'red' }, { color: 'blue' }, 'AppComponent');
-    expect(queryParams).toEqual({ color: 'red' });
-  });
+  describe('conversions', () => {
+    it('should warn when default query parameters does not contain a query param', () => {
+      jest.spyOn(console, 'warn').mockImplementation(() => undefined);
 
-  describe('number', () => {
-    it('should know how to transform whole numbers', () => {
-      const queryParams = convertQueryParamsToConcreteType({ id: '24' }, { id: 1 }, 'AppComponent');
-      expect(queryParams).toEqual({ id: 24 });
-    });
+      const location = { search: '?color=red' };
+      const defaultQueryParams = {};
 
-    it('should know how to transform fractured numbers', () => {
-      const queryParams = convertQueryParamsToConcreteType({ id: '1.12' }, { id: 33.3 }, 'AppComponent');
-      expect(queryParams).toEqual({ id: 1.12 });
-    });
-  });
+      const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+      expect(queryParams).toEqual({ color: 'red' });
 
-  describe('boolean', () => {
-    it('should know how to transform string "true" to boolean true', () => {
-      const queryParams = convertQueryParamsToConcreteType({ visible: 'true' }, { visible: true }, 'AppComponent');
-      expect(queryParams).toEqual({ visible: true });
-    });
-
-    it('should know how to transform string "false" to boolean false', () => {
-      const queryParams = convertQueryParamsToConcreteType({ visible: 'false' }, { visible: false }, 'AppComponent');
-      expect(queryParams).toEqual({ visible: false });
-    });
-  });
-
-  describe('array', () => {
-    it('should leave arrays of strings alone', () => {
-      const queryParams = convertQueryParamsToConcreteType(
-        { sizes: ['medium', 'large', 'small'] },
-        { sizes: ['small'] },
-        'AppComponent',
+      expect(console.warn).toHaveBeenCalledTimes(1);
+      expect(console.warn).toHaveBeenCalledWith(
+        `@42.nl/react-url: no default query param defined for "color" for: "AppComponent".`,
       );
-      expect(queryParams).toEqual({ sizes: ['medium', 'large', 'small'] });
     });
 
-    it('should know how to tranform arrays of numbers', () => {
-      const queryParams = convertQueryParamsToConcreteType(
-        { numbers: ['1', '2', '3'] },
-        { numbers: [42] },
-        'AppComponent',
-      );
-      expect(queryParams).toEqual({ numbers: [1, 2, 3] });
+    it('should leave strings and unhandled types alone', () => {
+      const location = { search: '?color=red' };
+      const defaultQueryParams = { color: 'blue' };
+
+      const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+      expect(queryParams).toEqual({ color: 'red' });
     });
 
-    it('should know how to tranform arrays of booleans', () => {
-      const queryParams = convertQueryParamsToConcreteType(
-        { visible: ['true', 'false', 'false'] },
-        { visible: [true] },
-        'AppComponent',
-      );
-      expect(queryParams).toEqual({ visible: [true, false, false] });
-    });
+    describe('number', () => {
+      it('should know how to transform whole numbers', () => {
+        const location = { search: '?id=42' };
+        const defaultQueryParams = { id: 1 };
 
-    it('should when it cannot guess the type, due to empty default, revert to strings', () => {
-      const queryParams = convertQueryParamsToConcreteType(
-        { sizes: ['medium', 'large', 'small'] },
-        { sizes: [] },
-        'AppComponent',
-      );
-      expect(queryParams).toEqual({ sizes: ['medium', 'large', 'small'] });
-    });
-
-    describe('when encountering a singular value', () => {
-      it('should convert to an array of strings when a singular string is given', () => {
-        const queryParams = convertQueryParamsToConcreteType({ sizes: 'large' }, { sizes: ['small'] }, 'AppComponent');
-        expect(queryParams).toEqual({ sizes: ['large'] });
+        const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+        expect(queryParams).toEqual({ id: 42 });
       });
 
-      it('should convert to an array of numbers when a singular number is given', () => {
-        const queryParams = convertQueryParamsToConcreteType({ numbers: '1' }, { numbers: [42] }, 'AppComponent');
-        expect(queryParams).toEqual({ numbers: [1] });
+      it('should know how to transform fractured numbers', () => {
+        const location = { search: '?id=1.12' };
+        const defaultQueryParams = { id: 33.3 };
+
+        const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+        expect(queryParams).toEqual({ id: 1.12 });
+      });
+    });
+
+    describe('boolean', () => {
+      it('should know how to transform string "true" to boolean true', () => {
+        const location = { search: '?visible=false' };
+        const defaultQueryParams = { visible: true };
+
+        const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+        expect(queryParams).toEqual({ visible: false });
       });
 
-      it('should convert to an array of booleans when a singular boolean is given', () => {
-        const queryParams = convertQueryParamsToConcreteType({ visible: 'true' }, { visible: [false] }, 'AppComponent');
-        expect(queryParams).toEqual({ visible: [true] });
+      it('should know how to transform string "false" to boolean false', () => {
+        const location = { search: '?visible=false' };
+        const defaultQueryParams = { visible: true };
+
+        const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+        expect(queryParams).toEqual({ visible: false });
+      });
+    });
+
+    describe('array', () => {
+      it('should leave arrays of strings alone', () => {
+        const location = { search: '?sizes=medium&sizes=large&sizes=small' };
+        const defaultQueryParams = { sizes: ['small'] };
+
+        const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+        expect(queryParams).toEqual({ sizes: ['medium', 'large', 'small'] });
       });
 
-      it('should convert to an array of the singular value when an empty default is given', () => {
-        const queryParams = convertQueryParamsToConcreteType({ sizes: 'medium' }, { sizes: [] }, 'AppComponent');
-        expect(queryParams).toEqual({ sizes: ['medium'] });
+      it('should know how to tranform arrays of numbers', () => {
+        const location = { search: '?numbers=1&numbers=2&numbers=3' };
+        const defaultQueryParams = { numbers: [42] };
+
+        const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+        expect(queryParams).toEqual({ numbers: [1, 2, 3] });
+      });
+
+      it('should know how to tranform arrays of booleans', () => {
+        const location = { search: '?visible=true&visible=false&visible=false' };
+        const defaultQueryParams = { visible: [true] };
+
+        const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+        expect(queryParams).toEqual({ visible: [true, false, false] });
+      });
+
+      it('should when it cannot guess the type, due to empty default, revert to strings', () => {
+        const location = { search: '?sizes=medium&sizes=large&sizes=small' };
+        const defaultQueryParams = { sizes: [] };
+
+        const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+        expect(queryParams).toEqual({ sizes: ['medium', 'large', 'small'] });
+      });
+
+      describe('when encountering a singular value', () => {
+        it('should convert to an array of strings when a singular string is given', () => {
+          const location = { search: '?sizes=large' };
+          const defaultQueryParams = { sizes: ['small'] };
+
+          const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+          expect(queryParams).toEqual({ sizes: ['large'] });
+        });
+
+        it('should convert to an array of numbers when a singular number is given', () => {
+          const location = { search: '?numbers=1' };
+          const defaultQueryParams = { numbers: [42] };
+
+          const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+          expect(queryParams).toEqual({ numbers: [1] });
+        });
+
+        it('should convert to an array of booleans when a singular boolean is given', () => {
+          const location = { search: '?visible=true' };
+          const defaultQueryParams = { visible: [false] };
+
+          const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+          expect(queryParams).toEqual({ visible: [true] });
+        });
+
+        it('should convert to an array of the singular value when an empty default is given', () => {
+          const location = { search: '?sizes=medium' };
+          const defaultQueryParams = { sizes: [] };
+
+          const queryParams = queryParamsFromLocation(location, defaultQueryParams, 'AppComponent');
+          expect(queryParams).toEqual({ sizes: ['medium'] });
+        });
       });
     });
   });
